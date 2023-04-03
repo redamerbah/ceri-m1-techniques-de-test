d959e1e7199bdcf3b0275225287fab740d4a7fcc
reda <redamerbah1999@gmail.com>
2023-04-03 15:16:16 +0200

    Test 16

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,4 +1,25 @@
 package fr.univavignon.pokedex.api;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mockito.Mockito;
 
 public interface IPokemonMetadataProviderTest {
+    @Test
+    public default void AffichageTest() throws PokedexException {
+        IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+
+        int indextest=133;
+        String name="Aquali";
+        int attack=186;
+        int defense = 168;
+        int stamina = 260;
+
+        PokemonMetadata pokemonMetadata = new PokemonMetadata(133, "Aquali", 186, 168, 260);
+        pokemonMetadataProvider.getPokemonMetadata(133);
+        assertEquals(name, pokemonMetadata.getName());
+        assertEquals(attack,pokemonMetadata.getAttack());
+        assertEquals(defense, pokemonMetadata.getDefense());
+        assertEquals(stamina, pokemonMetadata.getStamina());
+    }
 }
